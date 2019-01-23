a6bc214dfa4077f2f8fc01fca0e283a7e2c9a5c4
Mehedi Hasan <mehedi@mislbd.com>
2019-01-23 15:28:37 +0600

    fs auth done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,13 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { SuperSecretComponent } from './super-secret/super-secret.component';
 
-const config = {
-  apiKey: "AIzaSyBJmrs4Dg6naZ1cL9NRrOzdXSunL5taQU8",
-  authDomain: "fs-oauth-b0081.firebaseapp.com",
-  databaseURL: "https://fs-oauth-b0081.firebaseio.com",
-  projectId: "fs-oauth-b0081",
-  storageBucket: "fs-oauth-b0081.appspot.com",
-  messagingSenderId: "122215963160"
-};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperSecretComponent
   ],
   imports: [
     BrowserModule,
